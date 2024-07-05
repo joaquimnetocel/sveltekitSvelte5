4b0262f0f62ef675c2a0c2c6855cb0a8309213d4
joaquimnetocel <joaquim.neto.cel@gmail.com>
2024-07-05 11:28:22 -0300

    LET DIRECTIVE

---
+++ src/routes/let_directive/+page.svelte
@@ -0,0 +1,10 @@
+<!-- consumer -->
+<script>
+	import Button from './button.svelte';
+</script>
+
+<Button>
+	{#snippet children(prop)}
+		click {prop}
+	{/snippet}
+</Button>
---
+++ src/routes/let_directive/button.svelte
@@ -0,0 +1,16 @@
+<!-- provider -->
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let stateCounter = $state<number>(1);
+
+	let {
+		children,
+	}: {
+		children: Snippet<[number]>;
+	} = $props();
+</script>
+
+<button onclick={() => (stateCounter = stateCounter + 1)}>
+	{@render children(stateCounter)}
+</button>
